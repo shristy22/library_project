aee7d4c54a44483073ef83017019f62530a87b02
abhishek8290 <abhishek8290@gmail.com>
2020-08-14 02:25:17 +0530

    saving some space

---
+++ models/index.js
@@ -0,0 +1,9 @@
+let db = {};
+model.admin_schema = require('./admin_schema');
+model.book_rent_schema = require('./book_rent_schema');
+model.books_schema = require('./books_schema');
+model.customer_schema = require('./customer_schema');
+
+module.exports = model;
+
+
